34cb4c1699e15e0b856dd55b4f56919cb3e1e3d7
Ngoma Wise Crepin <132837143+wisecrepin4@users.noreply.github.com>
2025-06-10 23:18:16 +0200

    Update Sum of twoPositiveIntegers.js
    More definition of the working

--- 7kyu/Sum of twoPositiveIntegers.js
+++ 7kyu/Sum of twoPositiveIntegers.js
@@ -2,4 +2,5 @@ function sumTwoSmallestNumbers(numbers) {
   numbers = numbers.sort((a, b) => a - b);
   return numbers[0] + numbers[1];
 }
-//this give the summ of teh lowedst integets
+//this give the sum of teh lowest integets
+//by just adding those that came first because we had already committed
