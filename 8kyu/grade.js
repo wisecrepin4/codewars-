f723484afa65e6f9ebb79ef89ab6a22e73fc6c4e
wisecrepin <ngomawisecrepin@gmail.com>
2025-09-01 22:29:28 -0400

    renewal of the grade file

--- 8kyu/grade.js
+++ 8kyu/grade.js
@@ -1,19 +1,23 @@
-function grader(score) {
+function grade(score) {
   switch (true) {
+    case score > 1 || score < 0.6:
+      return "F";
+      break;
+
     case score >= 0.9:
       return "A";
       break;
+
     case score >= 0.8:
       return "B";
       break;
+
     case score >= 0.7:
       return "C";
       break;
+
     case score >= 0.6:
       return "D";
-      break;
-    case score > 1 || score < 0.6:
-      return "A";
       break;
   }
 }
